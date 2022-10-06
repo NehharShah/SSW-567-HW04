9530cc94f341c09d4fddbacba0064186b4ce95dd
Nihar Shah <84106256+NehharShah@users.noreply.github.com>
2022-10-06 18:51:15 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,10 @@
+"""
+Nihar Shah
+SSW-567: SW Testing, Qual. Assur. & Maint
+Fall 2022
+HW 04a: Develop with the Perspective of the Tester in mind
+"""
+
 GITHUB_URL = "https://api.github.com"
 USERS = "/users"
 REPOS = "/repos"
