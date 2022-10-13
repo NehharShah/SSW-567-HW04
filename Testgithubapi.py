d8152d11a068dee1e558fd1c4e05c592d5aa1ffa
NehharShah <nehharshah@gmail.com>
2022-10-12 23:31:07 -0400

    first commit

--- Testgithubapi.py
+++ Testgithubapi.py
@@ -1,20 +1,29 @@
-"""
-Nihar Shah
-SSW-567: SW Testing, Qual. Assur. & Maint
-Fall 2022
-HW 04a: Develop with the Perspective of the Tester in mind
-"""
-
+import json
 import unittest
+import unittest.mock
+from unittest.mock import patch
+from unittest.mock import Mock
+# from hw05a import getCommitnum, getUserRepos
 
-from githubapi import githubapi
+import githubapi
 
-class Testgithubapi(unittest.TestCase):
-    def testGithub(self):
-        self.assertEqual(githubapi('?'), False)
-    def testGithub2(self):
-        self.assertEqual(githubapi('NehharShah'), True)
+class TestHw04a(unittest.TestCase):
+        
+    @patch("githubapi.githubapi",return_value=['Repo: api-development-tools  Number of commits: 30', 'Repo: assets  Number of assets:30'])
+    #@patch.object(, "fetch_user_repo")
+    def test_fetch_user_repo_mock_api(self, mock_fetch_user_repo):
+        response_file = open('./response_fetch_user_repo.json')        
+        repo_call_response = json.load(response_file)
+        mock_fetch_user_repo.return_value = Mock(status_code = 200)
+        mock_fetch_user_repo.return_value.json = repo_call_response
+        response = githubapi.githubapi('NehharShah')
+        #print(response)
+        self.assertEqual(response.json[0]['name'],"chatbot_ner")
+        response_file.close()
+       
 
+
+        
 if __name__ == '__main__':
-    print("Test cases are running")
-    unittest.main()
+    print('Running unit tests')
+    unittest.main(exit=False)
